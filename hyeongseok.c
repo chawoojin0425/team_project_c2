5c149e4fb0ef3186c4dad2b0fb4d97c83d6a8f06
chawoojin0425 <163067232+chawoojin0425@users.noreply.github.com>
2024-05-12 19:22:37 +0900

    Update hyeongseok.c

--- hyeongseok.c
+++ hyeongseok.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct Student{
     char name[20]; 
     int id;                      
@@ -16,7 +15,7 @@ int main() {
     {"박지영", 20230003, 78, 83},
    };
    
-   FILE *fp = fopen("sj.txt", "wb");
+   FILE *fp = fopen("sj.txt", "w");
    if (fp == NULL) {
     printf("파일 열기 실패\n");
     return 1;
@@ -28,7 +27,7 @@ int main() {
    
    fclose(fp);
    
-    fp = fopen("sj.txt", "rb");
+    fp = fopen("sj.txt", "r");
     if (fp == NULL) {
         printf("파일 열기 실패\n");
         return 1;
@@ -38,18 +37,18 @@ int main() {
     
    fclose(fp);
 
-    float subject1_total = 0, subject2_total = 0;
+    float total1 = 0, total2 = 0;
     for (int i = 0; i < 3; i++) {
-        float individual_avg = (students[i].score1 + students[i].score2) / 2.0;
-        subject1_total += students[i].score1;
-        subject2_total += students[i].score2;
-        printf("%s (학번: %d): 개인 평균=%.2f\n", students[i].name, students[i].id, individual_avg);
+        float avg = (students[i].score1 + students[i].score2) / 2.0;
+        total1 += students[i].score1;
+        total2 += students[i].score2;
+        printf("%s (학번: %d): 개인 평균=%.2f\n", students[i].name, students[i].id, avg);
     }
 
-    float subject1_avg = subject1_total / 3;
-    float subject2_avg = subject2_total / 3;
+    float avg1 = total1 / 3;
+    float avg2 = total2 / 3;
 
-    printf("과목1 평균=%.2f, 과목2 평균=%.2f\n", subject1_avg, subject2_avg);
+    printf("과목1 평균=%.2f, 과목2 평균=%.2f\n", avg1, avg2);
 
     return 0;
 }
